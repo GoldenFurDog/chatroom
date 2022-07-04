c8eb10de70fe593d8290fabc170115bda7e5c47b
GoldenFurDog <77250048+GoldenFurDog@users.noreply.github.com>
2022-07-04 13:33:53 +0800

    Add files via upload

---
+++ chatroom_client.py
@@ -0,0 +1,19 @@
+#python3
+#chatroom_client v1.0.0
+#GoldenFurDog
+
+import socket
+import time
+
+
+client = socket.socket()
+ip = input("输入主机IP:")
+port = int(input("输入主机端口:"))
+print("已记录为%s:%s"%(ip,port))
+time.sleep(2)
+client.connect((ip,port))
+while True:
+    message = input("你 >>>")
+    client.send(message.encode())
+    redata = client.recv(1024)
+    print("对方 >>>" + redata.decode())
