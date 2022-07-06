5c78f92a34cb7221cce1b0b27c95a1a3df5c1f01
GoldenFurDog <77250048+GoldenFurDog@users.noreply.github.com>
2022-07-06 19:45:15 +0800

    Update server.py

--- server.py
+++ server.py
@@ -6,22 +6,26 @@ import socket
 
 server = socket.socket() 
 
-ip = input("键入本机IP地址(可用ifconfig,arp -a等命令获取):")
-port = int(input("键入空闲的端口(1~65535):"))
-print("已记录为%s:%s"%(ip,port))
+ip = input("输入本机IP地址(可用ifconfig,arp -a等命令获取):")
+port = int(input("输入空闲的端口(1~65535):"))
+name = input("请输入用户名:")
+print("已记录为%s:%s,用户名:%s"%(ip,port,name))
 server.bind((ip,port))
 server.listen(5)
+print("等待连接挂起。")
 while True:
     client,addr = server.accept()
     print("客户端发现:%s"%(str(addr)))
+    client.send(name.encode())
+    rname = client.recv(1024).decode()
     while True:
         redata = client.recv(1024)
         if redata == "stop":
             break
         else:
-            print("对方 >>>" + redata.decode())
+            print("%s >>>"%(rname) + redata.decode())
         
-        msg = input("你 >>>")
+        msg = input("%s >>>"%(name))
         if msg == "stop":
             break
         else:
